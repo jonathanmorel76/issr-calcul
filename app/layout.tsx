698a0dbe6bde19717037540dd31bae53872ccc4a
Jonathan Morel <jonathan.morel76@gmail.com>
2026-09-06 07:08:22 +0200

    style: load unified brand system

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Manrope } from 'next/font/google'
 import './globals.css'
 import './modern.css'
 import './dashboard.css'
+import './brand.css'
 
 const manrope = Manrope({ subsets: ['latin'], variable: '--font-app' })
 
