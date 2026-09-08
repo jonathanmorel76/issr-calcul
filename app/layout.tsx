34ee357ef8789bcd898c9b7eb2c8c74b90e7ab7a
Jonathan Morel <jonathan.morel76@gmail.com>
2026-09-08 08:01:30 +0200

    style: load premium journey stylesheet

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,7 @@ import './mobile-premium.css'
 import './indemnity-reconciliation-beta.css'
 import './premium-payment-overview-beta.css'
 import './premium-operational-insights-beta.css'
+import './premium-journey.css'
 import './mobile-final-polish.css'
 import './beta-mobile-corrections.css'
 
